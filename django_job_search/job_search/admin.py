00e2ff042ceec440f4dc9491c66975ba7aaeca4d
vladyslavtsurkan <vtsurkan2002@gmail.com>
2023-11-08 00:49:53 +0200

    Added translation in model fields, created JobAdmin class, overrides get_queryset method for JobAdmin

--- django_job_search/job_search/admin.py
+++ django_job_search/job_search/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from job_search.models import Job, Location, Organization, Degree, Spotlight
 
 admin.site.register(Spotlight)
-admin.site.register(Job)
 admin.site.register(Location)
 admin.site.register(Organization)
 admin.site.register(Degree)
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return Job.objects.select_related('organization')
--- django_job_search/job_search/models.py
+++ django_job_search/job_search/models.py
@@ -1,27 +1,28 @@
 from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from django_jsonform.models.fields import ArrayField
 
 
 class Spotlight(models.Model):
-    title = models.CharField('Title', max_length=50)
-    img = models.URLField('Image')
-    description = models.TextField()
+    title = models.CharField(_('Title'), max_length=50)
+    img = models.URLField(_('Image'))
+    description = models.TextField(_('Description'))
 
     def __str__(self):
         return self.title
 
 
 class Degree(models.Model):
-    name = models.CharField("Degree name", max_length=30, unique=True)
+    name = models.CharField(_("Degree name"), max_length=30, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Organization(models.Model):
-    name = models.CharField("Organization name", unique=True)
+    name = models.CharField(_("Organization name"), unique=True)
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -34,14 +35,14 @@ class Organization(models.Model):
 
 
 class Location(models.Model):
-    name = models.CharField("Location name", unique=True)
+    name = models.CharField(_("Location name"), unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Job(models.Model):
-    title = models.CharField("Job title", max_length=100)
+    title = models.CharField(_("Job title"), max_length=100)
 
     degree = models.ForeignKey(Degree, on_delete=models.CASCADE, related_name="jobs")
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="jobs")
@@ -52,7 +53,7 @@ class Job(models.Model):
     description = ArrayField(models.CharField(max_length=255))
 
     job_type = models.CharField(
-        "Job type",
+        _("Job type"),
         max_length=50,
         choices=[
             ("Full-time", "Full-time"),
