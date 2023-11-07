fdd2d545ffdc0984cc1fa8dac0c50f7ad49f0da6
vladyslavtsurkan <vtsurkan2002@gmail.com>
2023-11-07 14:54:37 +0200

    Modified JobSerializer, overrides methods create, update

--- django_job_search/job_search/api/serializers.py
+++ django_job_search/job_search/api/serializers.py
@@ -29,19 +29,72 @@ class LocationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class LocationNameSerializer(LocationSerializer):
-    def to_representation(self, instance):
-        return instance.name
-
-
 class JobSerializer(serializers.ModelSerializer):
-    locations = LocationNameSerializer(many=True)
+    locations = serializers.SlugRelatedField(
+        many=True,
+        slug_field='name',
+        queryset=Location.objects.all()
+    )
     degree = serializers.CharField(source='degree.name')
     organization = serializers.CharField(source='organization.name')
+
+    def create(self, validated_data):
+        degree_name = validated_data.pop('degree')['name']
+        organization_name = validated_data.pop('organization')['name']
+
+        degree = Degree.objects.get(name=degree_name)
+        organization = Organization.objects.get(name=organization_name)
+        location_names = validated_data.pop('locations')
+
+        job = Job.objects.create(
+            degree=degree,
+            organization=organization,
+            **validated_data
+        )
+
+        for location_name in location_names:
+            location, _ = Location.objects.get_or_create(name=location_name)
+            job.locations.add(location)
+
+        return job
+
+    def update(self, instance, validated_data):
+        job = Job.objects.get(pk=instance.id)
+
+        degree_data = validated_data.pop('degree', None)
+        organization_data = validated_data.pop('organization', None)
+
+        if degree_data is not None:
+            degree_name = degree_data.get('name')
+            degree = Degree.objects.get(name=degree_name)
+            validated_data['degree'] = degree
+
+        if organization_data is not None:
+            organization_name = organization_data.get('name')
+            organization = Organization.objects.get(name=organization_name)
+            validated_data['organization'] = organization
+
+        location_names = validated_data.pop('locations', None)
+
+        if location_names is not None:
+            locations = []
+            for location_name in location_names:
+                location, _ = Location.objects.get_or_create(name=location_name)
+                locations.append(location)
+
+            job.locations.set(locations)
+
+        for attr, value in validated_data.items():
+            setattr(job, attr, value)
+
+        job.save()
+
+        return job
 
     class Meta:
         model = Job
         fields = [
+            'id',
             'title',
             'degree',
             'locations',
