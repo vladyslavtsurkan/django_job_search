dd0045bef9df0a1f4d2bf31b7ecfebcac0092302
vladyslavtsurkan <vtsurkan2002@gmail.com>
2023-11-08 11:06:29 +0200

    Added Meta class to models and generated migration

--- django_job_search/job_search/models.py
+++ django_job_search/job_search/models.py
@@ -6,23 +6,31 @@ from django_jsonform.models.fields import ArrayField
 
 
 class Spotlight(models.Model):
-    title = models.CharField(_('Title'), max_length=50)
-    img = models.URLField(_('Image'))
-    description = models.TextField(_('Description'))
+    title = models.CharField(_('title'), max_length=50)
+    img = models.URLField(_('image'))
+    description = models.TextField(_('description'))
+
+    class Meta:
+        verbose_name = _('spotlight')
+        verbose_name_plural = _('spotlights')
 
     def __str__(self):
         return self.title
 
 
 class Degree(models.Model):
-    name = models.CharField(_("Degree name"), max_length=30, unique=True)
+    name = models.CharField(_("degree name"), max_length=30, unique=True)
+
+    class Meta:
+        verbose_name = _('degree')
+        verbose_name_plural = _('degrees')
 
     def __str__(self):
         return self.name
 
 
 class Organization(models.Model):
-    name = models.CharField(_("Organization name"), unique=True)
+    name = models.CharField(_("organization name"), unique=True)
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -30,19 +38,27 @@ class Organization(models.Model):
         null=False,
     )
 
+    class Meta:
+        verbose_name = _('organization')
+        verbose_name_plural = _('organizations')
+
     def __str__(self):
         return self.name
 
 
 class Location(models.Model):
-    name = models.CharField(_("Location name"), unique=True)
+    name = models.CharField(_("location name"), unique=True)
+
+    class Meta:
+        verbose_name = _('location')
+        verbose_name_plural = _('locations')
 
     def __str__(self):
         return self.name
 
 
 class Job(models.Model):
-    title = models.CharField(_("Job title"), max_length=100)
+    title = models.CharField(_("job title"), max_length=100)
 
     degree = models.ForeignKey(Degree, on_delete=models.CASCADE, related_name="jobs")
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="jobs")
@@ -53,7 +69,7 @@ class Job(models.Model):
     description = ArrayField(models.CharField(max_length=255))
 
     job_type = models.CharField(
-        _("Job type"),
+        _("job type"),
         max_length=50,
         choices=[
             ("Full-time", "Full-time"),
@@ -65,6 +81,10 @@ class Job(models.Model):
 
     date_added = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('job')
+        verbose_name_plural = _('jobs')
 
     def __str__(self):
         return f'{self.title} by {self.organization.name}'
