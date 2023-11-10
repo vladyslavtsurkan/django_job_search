e99b67484cd3d6fbaffb7ca030374ecefab8be69
vladyslavtsurkan <vtsurkan2002@gmail.com>
2023-11-10 23:15:42 +0200

    Added more tests for serializers

--- django_job_search/job_search/tests/test_serializers.py
+++ django_job_search/job_search/tests/test_serializers.py
@@ -1,15 +1,55 @@
 from django.test import TestCase
 
-from job_search.api.serializers import JobSerializer
+from job_search.api.serializers import (
+    JobSerializer,
+    LocationSerializer,
+    OrganizationSerializer,
+    DegreeSerializer,
+    SpotlightSerializer,
+)
 from job_search.models import Organization, Location, Degree
 from accounts.models import CustomUser
 
 
-class SerializersTestCase(TestCase):
+class SerializerTests(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email='test@example.com',
+            password='Hjsajk141'
+        )
+
+    def test_spotlight_serializer(self):
+        data = {
+            'title': 'Test Spotlight',
+            'img': 'http://example.com/image.jpg',
+            'description': 'Test Description'
+        }
+        serializer = SpotlightSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_degree_serializer(self):
+        data = {'name': 'Test Degree'}
+        serializer = DegreeSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_organization_serializer(self):
+        data = {'name': 'Test Organization'}
+        serializer = OrganizationSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+        # Test creator field read-only
+        serializer.validated_data['creator'] = self.user
+        instance = serializer.save()
+        self.assertEqual(instance.creator, self.user)
+
+    def test_location_serializer(self):
+        data = {'name': 'Test Location'}
+        serializer = LocationSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
     def test_job_serializer(self):
         degree = Degree.objects.create(name='Test Degree')
-        user = CustomUser.objects.create_user(email='test@example.com', password='Hjsajk141')
-        organization = Organization.objects.create(name='Test Organization', creator=user)
+        organization = Organization.objects.create(name='Test Organization', creator=self.user)
         location = Location.objects.create(name='Test Location')
 
         data = {
