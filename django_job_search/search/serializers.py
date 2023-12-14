4e20039005abab446f2ee86d938d886f23f5502c
vladyslavtsurkan <vtsurkan2002@gmail.com>
2023-12-14 23:31:01 +0200

    Modified JobDocumentSerializer for the same structure as job_search app API

--- django_job_search/search/serializers.py
+++ django_job_search/search/serializers.py
@@ -1,14 +1,23 @@
 from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
+from rest_framework import serializers
 
 from search.documents import JobDocument
 
 
 class JobDocumentSerializer(DocumentSerializer):
+    title = serializers.CharField(source='job_title')
+    degree = serializers.CharField(source='degree.name')
+    organization = serializers.CharField(source='organization.name')
+    locations = serializers.SerializerMethodField()
+
+    def get_locations(self, obj):
+        return [location.name for location in obj.locations]
+
     class Meta:
         document = JobDocument
         fields = (
             'id',
-            'job_title',
+            'title',
             'degree',
             'organization',
             'locations',
